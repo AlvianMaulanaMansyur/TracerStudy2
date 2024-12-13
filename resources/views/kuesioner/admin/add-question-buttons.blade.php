43090c7ea34c97cb94581558abfcf33457e3ff75
Suastika Putra <wsuastika50@gmail.com>
2024-12-13 12:00:01 +0800

    merge

---
+++ resources/views/kuesioner/admin/add-question-buttons.blade.php
@@ -0,0 +1,9 @@
+<div class="mb-4">
+    <h3 class="text-lg font-semibold mb-2">Tambahkan Pertanyaan</h3>
+    <button type="button" id="add-text-question"
+        class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded mr-2">Tambah Teks</button>
+    <button type="button" id="add-checkbox-question"
+        class="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded mr-2">Tambah Checkbox</button>
+    <button type="button" id="add-radio-question"
+        class="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded">Tambah Radio</button>
+</div>
