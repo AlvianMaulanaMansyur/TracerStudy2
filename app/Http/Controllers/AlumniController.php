37451b5d5b2181ec190569ec4f99dad8ec1a03ed
sesaputra <sesaputra243@gmail.com>
2025-01-20 23:18:25 +0800

    update statistik and landing page

--- app/Http/Controllers/AlumniController.php
+++ app/Http/Controllers/AlumniController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Alumni;
+use App\Models\Chart_data;
+use App\Models\Jawaban_kuesioner;
+use App\Models\Jawaban_logika;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Prodi;
@@ -57,7 +60,9 @@ class AlumniController extends Controller
             ->groupBy('angkatan')
             ->get();
 
-        return view('alumni.home', compact('jumlahAlumni', 'totalAlumni', 'angkatanPerJurusan', 'jumlahMahasiswaAktif', 'totalMahasiswaAktif', 'angkatanPerTahunLulus', 'angkatanPerTahun')); // Mengembalikan tampilan home alumni
+            $datastatusalumni = $this->getstatusalumni();
+
+        return view('alumni.home', compact('jumlahAlumni', 'totalAlumni', 'angkatanPerJurusan', 'jumlahMahasiswaAktif', 'totalMahasiswaAktif', 'angkatanPerTahunLulus', 'angkatanPerTahun', 'datastatusalumni')); // Mengembalikan tampilan home alumni
     }
     public function statistik()
     {
@@ -105,14 +110,134 @@ class AlumniController extends Controller
             ->groupBy('angkatan')
             ->get();
 
-        return view('alumni.statistik', compact('jumlahAlumni', 'totalAlumni', 'angkatanPerJurusan', 'jumlahMahasiswaAktif', 'totalMahasiswaAktif', 'angkatanPerTahunLulus', 'angkatanPerTahun')); // Mengembalikan tampilan home alumni
+            $charts = $this->getAllCharts();
+            $datastatusalumni = $this->getstatusalumni();
+
+            return view('alumni.statistik', compact('jumlahAlumni', 'totalAlumni', 'angkatanPerJurusan', 'jumlahMahasiswaAktif', 'totalMahasiswaAktif', 'angkatanPerTahunLulus', 'angkatanPerTahun', 'charts', 'datastatusalumni')); // Mengembalikan tampilan home alumni
     }
+
+    public function getstatusalumni()
+    {
+        // Ambil semua jawaban untuk pertanyaan tertentu
+        $jawabanAlumni = Jawaban_kuesioner::where('pertanyaan_id', 'Q006')->get();
+    
+        // Hitung jumlah jawaban "sudah" dan "belum"
+        $jumlahSudah = $jawabanAlumni->where('jawaban', 'sudah bekerja')->count();
+        $jumlahBelum = $jawabanAlumni->where('jawaban', 'belum bekerja')->count();
+    
+        // Kembalikan hasil dalam bentuk array atau objek
+        return [
+            'jumlah_sudah' => $jumlahSudah,
+            'jumlah_belum' => $jumlahBelum,
+        ];
+    }
+
+    public function getAllCharts()
+    {
+        // Ambil semua data chart
+        $chartDataRecords = Chart_data::all();
+
+        // Siapkan data untuk chart
+        $charts = [];
+
+        foreach ($chartDataRecords as $record) {
+            $dataChart = json_decode($record->data_chart, true);
+
+            // Ambil type dari data chart
+            $judulChart = $dataChart['judul_chart'];
+            $type = $dataChart['type'];
+            $chartType = $dataChart['chart_type'];
+
+            // Siapkan data untuk setiap chart
+            $chartData = [
+                'labels' => [],
+                'data' => []
+            ];
+
+            if ($type === 'pertanyaan') {
+                // Ambil jawaban dari tabel jawaban_kuesioner
+                $jawabanKuesioner = Jawaban_kuesioner::where('pertanyaan_id', $dataChart['id'])->get();
+                $opsiJawaban = $dataChart['opsi_jawaban'];
+
+                if (!empty($opsiJawaban)) {
+                    foreach ($opsiJawaban as $opsi) {
+                        $jawabanCount = $jawabanKuesioner->where('jawaban', $opsi)->count();
+                        // Cek apakah opsi sudah ada di labels
+                        $index = array_search($opsi, $chartData['labels']);
+                        if ($index !== false) {
+                            // Jika sudah ada, tambahkan jumlahnya
+                            $chartData['data'][$index] += $jawabanCount;
+                        } else {
+                            // Jika belum ada, tambahkan ke labels dan data
+                            $chartData['labels'][] = $opsi;
+                            $chartData['data'][] = $jawabanCount;
+                        }
+                    }
+                } else {
+                    foreach ($jawabanKuesioner as $jawaban) {
+                        // Cek apakah jawaban sudah ada di labels
+                        $index = array_search($jawaban->jawaban, $chartData['labels']);
+                        if ($index !== false) {
+                            // Jika sudah ada, tambahkan jumlahnya
+                            $chartData['data'][$index] += 1;
+                        } else {
+                            // Jika belum ada, tambahkan ke labels dan data
+                            $chartData['labels'][] = $jawaban->jawaban;
+                            $chartData['data'][] = 1;
+                        }
+                    }
+                }
+            } elseif ($type === 'logika') {
+                // Ambil jawaban dari tabel jawaban_logika
+                $jawabanLogika = Jawaban_logika::where('logika_id', $dataChart['id'])->get();
+                $opsiJawaban = $dataChart['opsi_jawaban'];
+
+                if (!empty($opsiJawaban)) {
+                    foreach ($opsiJawaban as $opsi) {
+                        $jawabanCount = $jawabanLogika->where('jawaban', $opsi)->count();
+                        // Cek apakah opsi sudah ada di labels
+                        $index = array_search($opsi, $chartData['labels']);
+                        if ($index !== false) {
+                            // Jika sudah ada, tambahkan jumlahnya
+                            $chartData['data'][$index] += $jawabanCount;
+                        } else {
+                            // Jika belum ada, tambahkan ke labels dan data
+                            $chartData['labels'][] = $opsi;
+                            $chartData['data'][] = $jawabanCount;
+                        }
+                    }
+                } else {
+                    foreach ($jawabanLogika as $jawaban) {
+                        // Cek apakah jawaban sudah ada di labels
+                        $index = array_search($jawaban->jawaban, $chartData['labels']);
+                        if ($index !== false) {
+                            // Jika sudah ada, tambahkan jumlahnya
+                            $chartData['data'][$index] += 1;
+                        } else {
+                            // Jika belum ada, tambahkan ke labels dan data
+                            $chartData['labels'][] = $jawaban->jawaban;
+                            $chartData['data'][] = 1;
+                        }
+                    }
+                }
+            }
+            $kuesionerId = $record->kuesioner_id; // Pastikan ada kolom kuesioner_id di Chart_data
+            $charts[$kuesionerId][] = [
+                'id' => $record->id,
+                'data' => $chartData,
+                'chartType' => $chartType,
+                'title' => $judulChart,
+            ];
+        }
+
+        return $charts;
+    }
+
     public function faq()
     {
         // Anda bisa mengambil data alumni dari database jika diperlukan
         // $alumni = Alumni::all(); // Contoh pengambilan data alumni
 
         return view('alumni.faq'); // Mengembalikan tampilan home alumni
-    } 
-    
+    }
 }
